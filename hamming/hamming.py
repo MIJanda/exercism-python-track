d7a7279a35d4db4f062a65ee537289407e22d83d
MIJanda <mokiliisaacjanda@gmail.com>
2020-11-30 19:28:28 +0300

    Initial commit of Python track exercism programming exercises

---
+++ hamming/hamming.py
@@ -0,0 +1,12 @@
+def distance(strand_a, strand_b):
+    if (len(strand_a) != len(strand_b)):
+        raise ValueError('The strands are not the same length')
+
+    count = 0
+        
+    for i in range(len(strand_a)):
+        if strand_a[i] != strand_b[i]:
+            count += 1
+        
+
+    return count 
