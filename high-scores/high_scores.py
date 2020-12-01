abefaea7eeee1191a09b65482a56db2774412e64
MIJanda <mokiliisaacjanda@gmail.com>
2020-12-01 13:58:23 +0300

    completed the high scores exercise

---
+++ high-scores/high_scores.py
@@ -0,0 +1,20 @@
+def latest(scores):
+    return scores[len(scores) - 1]
+
+
+def personal_best(scores):
+    return max(scores)
+
+
+def personal_top_three(scores):
+    scores.sort()
+    scores.reverse()
+
+    if (len(scores) >= 3):
+        return scores[:3]  
+    else:
+        return scores 
+                   
+
+
+        
